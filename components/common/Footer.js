98d3b58c065e457ba0ce4eeabadb869cfc08f7d7
Sep Motameni <me@sepmotameni.com>
2022-06-20 01:27:30 -0400

    removed banner loop and added padding to footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-center h-12 space-x-3 bg-[#141414] md:h-24">
+    <footer className="flex items-center justify-center space-x-3 bg-[#141414] py-10">
       <img
         className="border border-gray-400 rounded-full w-9 h-9 md:w-12 md:h-12 border-3"
         src="/sep.png"
