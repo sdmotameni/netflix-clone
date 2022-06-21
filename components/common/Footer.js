be6cbb964ae4d0f71fd590bd938347823a972974
Sep Motameni <me@sepmotameni.com>
2022-06-21 00:05:36 -0400

    Removed footer animation and added head to list

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,37 +1,14 @@
-import { motion } from "framer-motion";
-
 export default function Footer() {
-  const footerVariants = {
-    hidden: {
-      scale: 2,
-      y: -1500,
-    },
-    visible: {
-      scale: 1,
-      y: 0,
-      transition: {
-        duration: 4,
-      },
-    },
-  };
-
   return (
-    <footer className="bg-[#141414] py-10">
-      <motion.span
-        className="flex items-center justify-center space-x-3"
-        initial="hidden"
-        animate="visible"
-        variants={footerVariants}
-      >
-        <img
-          className="border border-gray-400 rounded-full w-9 h-9 md:w-12 md:h-12 border-3"
-          src="/sep.png"
-          alt=""
-        />
-        <p className="text-xs text-white text-gray-400 md:text-sm">
-          Developed by <span className="font-semibold">Sep Motameni</span>
-        </p>
-      </motion.span>
+    <footer className="bg-[#141414] py-10 flex items-center justify-center space-x-3">
+      <img
+        className="border border-gray-400 rounded-full w-9 h-9 md:w-12 md:h-12 border-3"
+        src="/sep.png"
+        alt=""
+      />
+      <p className="text-xs text-white text-gray-400 md:text-sm">
+        Developed by <span className="font-semibold">Sep Motameni</span>
+      </p>
     </footer>
   );
 }
--- pages/mylist.js
+++ pages/mylist.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Navbar from "../components/Navbar";
 import Movies from "../components/Movies";
 import Footer from "../components/common/Footer";
@@ -5,6 +7,11 @@ import Footer from "../components/common/Footer";
 export default function MyList({ movies = {} }) {
   return (
     <>
+      <Head>
+        <title>My List - Netflix Clone by Sep Motameni</title>
+        <meta name="description" content="Netflix Clone by Sep Motameni" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="min-h-screen bg-[#141414]">
         <Navbar currentPage="mylist" />
         <Movies movies={movies} />
